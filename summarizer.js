1a0044c58da8fc8b2118cdc2a59d303ed018e896
Erick <erickungu82@gmail.com>
2024-02-01 10:15:29 +0300

    Create an async function summarizeText that will  be used to call the hugging face API.

---
+++ summarizer.js
@@ -0,0 +1,7 @@
+// This is the function where the call to the API is made. Returns the summarized text as a string.
+
+async function summarizeText(text) {
+
+    // INSERT CODE SNIPPET FROM POSTMAN BELOW
+
+}
