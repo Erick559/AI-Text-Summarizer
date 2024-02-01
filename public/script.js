b2ea3fea422edf78c8c40ed29de10247f27b259b
Erick <erickungu82@gmail.com>
2024-02-02 00:08:22 +0300

    Add an event listener to the text area to check that any text in the input textarea is a valid length each time text is changed.

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
 const textArea = document.getElementById('text_to_summarize');
 const submitButton = document.getElementById('submit-button')
 const summarizedTextArea = document.getElementById('summary');
+
+textArea.addEventListener('input', verifyTextLength)
