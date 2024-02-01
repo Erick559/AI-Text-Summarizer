53a24f261669c03f66f2f789cef7f387416cfd2c
Erick <erickungu82@gmail.com>
2024-02-01 10:57:28 +0300

    Import axios.

--- summarizer.js
+++ summarizer.js
@@ -1,9 +1,9 @@
-// This is the function where the call to the API is made. Returns the summarized text as a string.
+const axios = require('axios');
 
+// This is the function where the call to the API is made. Returns the summarized text as a string.
 async function summarizeText(text) {
 
     // INSERT CODE SNIPPET FROM POSTMAN BELOW
-    const axios = require('axios');
     let data = JSON.stringify({
         "inputs": "Amelia, a curious astronomer, spotted a peculiar comet through her telescope. It emitted a mesmerizing glow and seemed to change course. Determined to investigate, she built a spaceship. On her journey, she encountered cosmic wonders, yet the comet remained elusive. Finally, at the edge of the universe, she understood its secret: the comet was a celestial guide, leading her to discover the boundless wonders within herself. Grateful for the voyage, Amelia returned, sharing her newfound wisdom with the world, inspiring others to explore both the cosmos and the depths of their souls.",
         "parameters": {
